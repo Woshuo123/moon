3a970dce49d3b6e21df341d65cca0a7ef764582b
Woshuo123 <1015251222@qq.com>
2022-08-22 15:20:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 
 today = datetime.now()
 
-start_date = os.environ['BEGIN_DAY']
 city = os.environ['CITY']
+start_date = os.environ['START_DATE']
 app_id = os.environ['APP_ID']
 app_secret = os.environ['APP_SECRET']
 user_id = os.environ["USER_ID"]
